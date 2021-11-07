4c3bec24374b6c7a98a0f92632699cf39e5eeaec
fetobk <42487137+fetobk@users.noreply.github.com>
2021-11-07 11:13:56 +0800

    Add files via upload

--- Http.py
+++ Http.py
@@ -1,5 +1,6 @@
 import urllib.request
-
+from urllib.error import URLError
+from urllib.request import ProxyHandler,build_opener
 
 def get(url: str, header: list = {}):
     """
@@ -8,10 +9,19 @@ def get(url: str, header: list = {}):
     :param header: HTTP头
     :return: row
     """
+    proxy='127.0.0.1:10809' 
+    proxy_handler=ProxyHandler({
+        'http':'http://'+proxy,
+        'https':'https://'+proxy
+    })
+    opener=build_opener(proxy_handler)
+    urllib.request.install_opener(opener)
+    print('aaa')
     header['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
     header['Accept-Language'] = 'zh-CN,zh;q=0.8,en;q=0.6'
     header['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3790.0 Safari/537.36'
     retry_count = 4
+    print ('bbb')
     while retry_count:
         req = urllib.request.urlopen(urllib.request.Request(url, headers=header))
         try:
